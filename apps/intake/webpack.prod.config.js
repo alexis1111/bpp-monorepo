d0dc0ef7b35040b96509c71e9198e776fec61267
Alexis Escudero <alexisescudero1@gmail.com>
2022-12-28 17:08:33 -0300

    changes

--- apps/intake/webpack.prod.config.js
+++ apps/intake/webpack.prod.config.js
@@ -2,6 +2,12 @@ const { withModuleFederation } = require('@nrwl/angular/module-federation');
 const config = require('./module-federation.config');
 module.exports = withModuleFederation({
   ...config,
+  remotes: [
+    ['auth', 'https://exquisite-paletas-ea1d46.netlify.app'],
+    ['core', 'https://exquisite-paletas-ea1d46.netlify.app'],
+    ['intake', 'https://exquisite-paletas-ea1d46.netlify.app'],
+    ['panel', 'https://exquisite-paletas-ea1d46.netlify.app'],
+ ]
   /*
    * Remote overrides for production.
    * Each entry is a pair of an unique name and the URL where it is deployed.
